325976617ab1968ef7b403c8a19a93f1aa6dbe1c
YuHyeonGeun-KOR <63186208+YuHyeonGeun-KOR@users.noreply.github.com>
2023-05-21 15:25:23 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT o.ANIMAL_ID as ANIMAL_ID , o.NAME as NAME
+from ANIMAL_INS AS i RIGHT OUTER JOIN ANIMAL_OUTS as o  on i.ANIMAL_ID = o.ANIMAL_ID
+where i.ANIMAL_ID is null
+order by o.ANIMAL_ID
